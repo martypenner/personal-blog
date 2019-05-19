a40909212ce23e8a7ef3dc4f76d398dfe29bad8a
Marty Penner <penner.marty@gmail.com>
2019-05-18 22:43:02 -0400

    Add my blog posts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -29,7 +29,7 @@ const Template = props => {
           }}
           to={'/'}
         >
-          Gatsby Starter Blog
+          Marty's Blog
         </Link>
       </h1>
     )
@@ -50,7 +50,7 @@ const Template = props => {
           }}
           to={'/'}
         >
-          Gatsby Starter Blog
+          Marty's Blog
         </Link>
       </h3>
     )
