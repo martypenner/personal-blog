060f8588d6090f3cf160044bfe0c0bd98c3f76c4
Marty Penner <penner.marty@gmail.com>
2022-02-13 00:01:02 -0500

    feat: use swyxkit for better blog!

---
+++ src/lib/siteConfig.js
@@ -0,0 +1,14 @@
+export const SITE_URL = 'https://penner.me';
+export const GH_USER_REPO = 'martypenner/personal-blog'; // used for pulling github issues and offering comments
+export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
+export const SITE_TITLE = 'My Blog';
+export const SITE_DESCRIPTION =
+	'My personal blog, because my keystrokes are too valuable to waste.';
+export const DEFAULT_OG_IMAGE =
+	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
+export const MY_TWITTER_HANDLE = 'MartyPenner';
+export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
+
+// dont forget process.env.GH_TOKEN
+// if supplied, raises rate limit from 60 to 5000
+// https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
