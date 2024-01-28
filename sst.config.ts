769aec79b5751244fd3d8cce07669cf5e13de4f5
Marty Penner <martypenner@users.noreply.github.com>
2024-01-27 22:48:27 -0500

    chore: auto-gen cert

--- sst.config.ts
+++ sst.config.ts
@@ -1,9 +1,6 @@
 import type { SSTConfig } from 'sst';
-import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
 import { SvelteKitSite } from 'sst/constructs';
-
-const certArn =
-	'arn:aws:acm:us-east-1:360597683419:certificate/127937f8-c344-4911-8342-f4333d154006';
 
 export default {
 	config(_input) {
@@ -14,15 +11,22 @@ export default {
 	},
 	stacks(app) {
 		app.stack(function Site({ stack }) {
+			const certificate = new Certificate(this, 'Certificate', {
+				domainName: '*.penner.me',
+				subjectAlternativeNames: ['penner.me'],
+				validation: CertificateValidation.fromDns()
+			});
+
 			const site = new SvelteKitSite(stack, 'site', {
 				customDomain: {
 					domainName: 'penner.me',
 					isExternalDomain: true,
 					cdk: {
-						certificate: Certificate.fromCertificateArn(stack, 'MyCert', certArn)
+						certificate
 					}
 				}
 			});
+
 			stack.addOutputs({
 				url: site.url
 			});
